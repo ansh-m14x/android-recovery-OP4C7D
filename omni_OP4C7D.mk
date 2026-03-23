25031f8e55627480477c719841aa12a9f508f22b
ansh-m14x <ansh981786@gmail.com>
2026-03-23 19:11:47 +0530

    Update omni_OP4C7D.mk

--- omni_OP4C7D.mk
+++ omni_OP4C7D.mk
@@ -1,13 +1,28 @@
-# Inherit basic Android build targets
+# Release name
+PRODUCT_RELEASE_NAME := OP4C7D
+
+# Inherit from Android build targets
+$(call inherit-product, build/target/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
 
-# PBRP Android 9.0 ke liye hum missing common.mk ki line hata rahe hain
-# Ye build system ko error dene se rokega.
+# Inherit from PitchBlack common configuration
+$(call inherit-product, vendor/pb/config/common.mk)
 
+## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := OP4C7D
 PRODUCT_NAME := omni_OP4C7D
 PRODUCT_BRAND := oppo
 PRODUCT_MODEL := CPH2015
 PRODUCT_MANUFACTURER := oppo
-PRODUCT_RELEASE_NAME := Oppo A31 (PitchBlack)
+
+# HACK: Set vendor patch level to bypass anti-rollback
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2099-12-31
+
+# Blacklist to prevent build date mismatches
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
+    ro.product.device \
+    ro.product.name \
+    ro.build.product \
+    ro.bootimage.build.date.utc \
+    ro.build.date.utc
