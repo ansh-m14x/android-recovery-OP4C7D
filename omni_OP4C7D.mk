1cc2f37e5b4b5c9c65f2ec7ad740f233838d5733
ansh-m14x <ansh981786@gmail.com>
2026-03-23 18:41:48 +0530

    Update omni_OP4C7D.mk

--- omni_OP4C7D.mk
+++ omni_OP4C7D.mk
@@ -1,24 +1,17 @@
-#
-# Copyright (C) 2019 The Android Open Source Project
-# Copyright (C) 2019 The TWRP Open Source Project
-# Copyright (C) 2026 Ansh_m14x (Updated for OrangeFox/PBRP)
-#
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
 
-# Inherit from our custom product configuration
-# Note: OrangeFox uses vendor/twrp/config/common.mk for minimal builds
-$(call inherit-product, vendor/twrp/config/common.mk)
+# PBRP Specific Inheritance
+$(call inherit-product, vendor/pbrp/config/common.mk)
 
-# Device identifier. Full name of the product.
+# Device identifier
 PRODUCT_DEVICE := OP4C7D
 PRODUCT_NAME := omni_OP4C7D
 PRODUCT_BRAND := oppo
 PRODUCT_MODEL := CPH2015
 PRODUCT_MANUFACTURER := oppo
-PRODUCT_RELEASE_NAME := Oppo A31
+PRODUCT_RELEASE_NAME := Oppo A31 (PitchBlack)
 
 # Build fingerprints & Description
 PRODUCT_BUILD_PROP_OVERRIDES += \
@@ -26,7 +19,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_PRODUCT=OP4C7D \
     TARGET_DEVICE=OP4C7D
 
-# Display Dimensions (Oppo A31 Actual: 720x1600)
+# Display Dimensions
 TARGET_SCREEN_HEIGHT := 1600
 TARGET_SCREEN_WIDTH := 720
-TV_THEME := portrait_hdpi
+TW_THEME := portrait_hdpi
