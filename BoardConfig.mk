2c177cdf25466c29036450b54bcb78e438b7122c
ansh-m14x <ansh981786@gmail.com>
2026-03-23 14:51:53 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,28 +1,9 @@
-#
-# Copyright (C) 2019 The Android Open Source Project
-# Copyright (C) 2019 The TWRP Open Source Project
-# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator 
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 DEVICE_PATH := device/oppo/OP4C7D
-LOCAL_PATH := device/oppo/OP4C7D
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
 
-# Assert
+# Assert & System Root
 TARGET_OTA_ASSERT_DEVICE := CPH2015, CPH2081, OP4C7D
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 
@@ -31,114 +12,74 @@ TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_VARIANT := generic
 
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a53
+TARGET_2ND_CPU_VARIANT := generic
 
-TARGET_CPU_ABI_LIST := arm64-v8a,armeabi-v7a,armeabi
-TARGET_CPU_ABI_LIST_64_BIT := arm64-v8a
-TARGET_CPU_ABI_LIST_32_BIT := armeabi-v7a,armeabi
-
-TARGET_BOARD_SUFFIX := _64
 TARGET_USES_64_BIT_BINDER := true
 
-# Kernel - Reordered for build reliability
+# Kernel Configuration
 BOARD_BOOTIMG_HEADER_VERSION := 1
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_OFFSET := 0x00008000
 BOARD_RAMDISK_OFFSET := 0x11a88000
-BOARD_SECOND_OFFSET := 0x00e88000
-BOARD_KERNEL_TAGS_OFFSET := 0x07808000
+BOARD_TAGS_OFFSET := 0x07808000
 
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive loop.max_part=7
 BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_TAGS_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 
+# Prebuilt Kernel & DTBO (Matching your 'prebuilt' folder names)
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/image.gz-dtb
 BOARD_KERNEL_IMAGE_NAME := image.gz-dtb
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
 BOARD_INCLUDE_RECOVERY_DTBO := true
 
-TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_HEADER_ARCH := arm64
-
 # Platform
 TARGET_BOARD_PLATFORM := mt6765
-TARGET_BOARD_SUFFIX := _64
-TARGET_USES_64_BIT_BINDER := true
-TARGET_IS_64_BIT := true
-
-# MTK Hardware
 BOARD_HAS_MTK_HARDWARE := true
-BOARD_USES_MTK_HARDWARE := true
 MTK_HARDWARE := true
 
 # Partitions
-BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 33554432
-BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_CACHEIMAGE_PARTITION_SIZE := 452984832
-BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 6442450944
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 22105013760
 BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 33554432
+BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 6442450944
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 22105013760
 
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
-
-# Workaround for error copying vendor files to recovery ramdisk
-TARGET_COPY_OUT_VENDOR := vendor
-
-# Recovery
+# Recovery UI Settings
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery.fstab
 BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_SUPPRESS_SECURE_ERASE := true
-BOARD_USE_FRAMEBUFFER_ALPHA_CHANNEL := true
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-TARGET_DISABLE_TRIPLE_BUFFERING := false
-RECOVERY_SDCARD_ON_DATA := true
-
-# TWRP Configuration
 TW_THEME := portrait_hdpi
-TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_DEVICE_VERSION := 1_BY_Ansh_m14x
-TW_MAX_BRIGHTNESS := 4095
-TW_DEFAULT_BRIGHTNESS := 1024
-TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
-TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
-TW_USE_TOOLBOX := true
-TW_NO_SCREEN_BLANK := true
-TW_NO_BATT_PERCENT := false
-TW_EXCLUDE_TWRPAPP := true
-TW_EXCLUDE_SUPERSU := true
 TW_EXTRA_LANGUAGES := true
-TW_DEFAULT_LANGUAGE := en
-TW_OZIP_DECRYPT_KEY := "ACAC1E13A72531AE4A1B22BB31C1CC22"
-TW_EXCLUDE_DEFAULT_USB_INIT := true
-ENABLE_CPUSETS := true
-ENABLE_SCHEDBOOST := true
 TW_SCREEN_BLANK_ON_BOOT := true
-TW_SKIP_COMPATIBILITY_CHECK := true
-TW_Y_OFFSET := 52
-TW_H_OFFSET := -52
 
-# Hack: prevent anti rollback
+# OrangeFox / PBRP Flags
+FOX_VERSION := R11.1
+FOX_BUILD_TYPE := Unofficial
+FOX_MAINER := Ansh_m14x
+TARGET_DEVICE_ALT := OP4C7D, CPH2015, CPH2081, oppo6765
+FOX_RECOVERY_INSTALLER_PATH := $(DEVICE_PATH)/prebuilt
+FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER := true
+FOX_REPLACE_TOOLBOX_GETPROP := 1
+FOX_USE_TAR_BINARY := 1
+FOX_USE_ZIP_BINARY := 1
+FOX_USE_LZMA_COMPRESSION := 1
+
+# OZIP Decryption (Special for Oppo)
+TW_OZIP_DECRYPT_KEY := "ACAC1E13A72531AE4A1B22BB31C1CC22"
+
+# Anti-Rollback Hack
 PLATFORM_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 16.1.0
 
 # Debugging
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
-
-# Avb - Disabled for Custom Recovery Stability
-BOARD_AVB_ENABLE := false
