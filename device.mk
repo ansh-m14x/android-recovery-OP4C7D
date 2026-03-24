63a83d4f2164dc8d2b7c3a12ba2243373102c17e
ansh-m14x <ansh981786@gmail.com>
2026-03-24 23:17:55 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,20 +1,23 @@
-# Rootdir files
+# Rootdir files (Basic Graphics for Recovery)
 PRODUCT_PACKAGES += \
     libfwdlockengine \
     libGLESv2 \
     libGLESv3
 
-# Kernel Modules & Prebuilt Copy
+# Kernel & DTBO Copy (Match your prebuilt folder name)
+# Agar folder ka naam 'prebuilt' hai toh ye sahi hai, 
+# lekin aksar hum 'prebuilts' (s ke saath) use karte hain. Check kar lena.
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/image.gz-dtb:kernel \
-    $(LOCAL_PATH)/prebuilt/dtbo.img:dtbo.img
+    $(LOCAL_PATH)/prebuilts/Image.gz-dtb:kernel \
+    $(LOCAL_PATH)/prebuilts/dtbo.img:dtbo.img
 
-# Recovery fstab
+# Recovery fstab (Sahi path for LineageOS)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery.fstab:recovery/root/system/etc/recovery.fstab
 
-# Default properties
+# Debugging & ADB (Inse recovery mein ADB access milega)
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.adb.secure=0 \
     ro.debuggable=1 \
+    ro.secure=0 \
     persist.sys.usb.config=mtp,adb
