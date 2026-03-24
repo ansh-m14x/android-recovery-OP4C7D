215d69ce2c1d01f1121e5d4ce79b9e946f0f969a
ansh-m14x <ansh981786@gmail.com>
2026-03-24 08:58:17 +0530

    Update omni_OP4C7D.mk

--- omni_OP4C7D.mk
+++ omni_OP4C7D.mk
@@ -7,22 +7,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit from PitchBlack common configuration
 $(call inherit-product, vendor/pb/config/common.mk)
-
-## Device identifier. This must come after all inclusions
+# --- Set PBRP Overwrites (Sample Fix) ---
+BOARD_VENDOR := oppo
+PRODUCT_BRAND := oppo
 PRODUCT_DEVICE := OP4C7D
 PRODUCT_NAME := omni_OP4C7D
-PRODUCT_BRAND := oppo
-PRODUCT_MODEL := CPH2015
 PRODUCT_MANUFACTURER := oppo
+PRODUCT_MODEL := CPH2015
+TARGET_VENDOR := oppo
 
-# HACK: Set vendor patch level to bypass anti-rollback
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.security_patch=2099-12-31
-
-# Blacklist to prevent build date mismatches
-PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
-    ro.product.device \
-    ro.product.name \
-    ro.build.product \
-    ro.bootimage.build.date.utc \
-    ro.build.date.utc
+PRODUCT_RELEASE_NAME := Oppo A31
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
