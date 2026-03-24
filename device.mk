90c31ee2f51a2aaa9f5efa20903c294dd5d5e320
ansh-m14x <ansh981786@gmail.com>
2026-03-24 23:41:25 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -4,18 +4,16 @@ PRODUCT_PACKAGES += \
     libGLESv2 \
     libGLESv3
 
-# Kernel & DTBO Copy (Match your prebuilt folder name)
-# Agar folder ka naam 'prebuilt' hai toh ye sahi hai, 
-# lekin aksar hum 'prebuilts' (s ke saath) use karte hain. Check kar lena.
+# Kernel & DTBO Copy (Synced with Folder Name: prebuilt)
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilts/Image.gz-dtb:kernel \
-    $(LOCAL_PATH)/prebuilts/dtbo.img:dtbo.img
+    $(LOCAL_PATH)/prebuilt/image.gz-dtb:kernel \
+    $(LOCAL_PATH)/prebuilt/dtbo.img:dtbo.img
 
-# Recovery fstab (Sahi path for LineageOS)
+# Recovery fstab
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery.fstab:recovery/root/system/etc/recovery.fstab
 
-# Debugging & ADB (Inse recovery mein ADB access milega)
+# Default properties (ADB & Debugging Enabled)
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.adb.secure=0 \
     ro.debuggable=1 \
