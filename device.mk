47944db16dcbc1f7c761493257fa601be8421453
ansh-m14x <ansh981786@gmail.com>
2026-03-25 11:43:54 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -12,6 +12,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init.mt6765.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.mt6765.rc \
     $(LOCAL_PATH)/init.mt6765.usb.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.mt6765.usb.rc
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/default.prop:$(TARGET_COPY_OUT_RAMDISK)/default.prop
+
 # Extra: Agar first stage mount chahiye (Optional but safer for MTK)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery.fstab:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.mt6765
